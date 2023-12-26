d116678fa0d400e39ea8ab7440e02ed5830d35ce
Ywissam <152511493+Ywissam@users.noreply.github.com>
2023-12-26 20:25:51 +0100

    Update C-project.c

--- C-project.c
+++ C-project.c
@@ -3,22 +3,22 @@
 //declaration
 
  typedef struct Liste Liste;
- typedef Liste * Listo;
+ typedef Liste * ListP;
    struct Liste {
   int info;
-     Listo  suiv;
-     Listo pre;
+     ListP  suiv;
+     ListP pre;
    } ;
 
-Listo creerElement(int valeur) { 
-    Listo nouvelElement = (Listo) malloc(sizeof(Liste));
+ListP creerElement(int valeur) { 
+    ListP nouvelElement = (ListP) malloc(sizeof(Liste));
     nouvelElement->info = valeur;
     nouvelElement->suiv = NULL;
     nouvelElement->pre = NULL;
     return nouvelElement;
 
-void afficherliste(Listo L){ //du premier vers le dernier
-    Listo p;
+void afficherliste(ListP L){ //du premier vers le dernier
+    ListP p;
     p=L;
     printf("l'affichage de la liste du premier element vers le dernier");
     printf("debuuut->");
