3877f9bc60572944941d9626d1e7b9df512df5b9
djelidlyna <133454749+djelidlyna@users.noreply.github.com>
2023-12-26 12:40:45 +0100

    ajout de la fonction afficher du premier vers le dernier

--- C-project.c
+++ C-project.c
@@ -9,3 +9,18 @@
      Listo  suiv;
      Listo pre;
    } ;
+void afficherliste(Listo L){ //du premier vers le dernier
+    Listo p;
+    p=L;
+    printf("l'affichage de la liste du premier element vers le dernier");
+    printf("debuuut->");
+    while(p!=NULL){
+        printf("%d",p->info);
+        p=p->suiv;
+    }
+    printf("Null");
+}
+ int main(){
+
+  return 0;
+ }
