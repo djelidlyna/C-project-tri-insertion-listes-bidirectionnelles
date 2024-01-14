b043c3750e10672dc048a413795efe3b1af0287b
Ywissam <152511493+Ywissam@users.noreply.github.com>
2024-01-14 18:44:27 +0100

    insertElementAtPosition

--- C-project.c
+++ C-project.c
@@ -36,7 +36,51 @@ void addElement(ListP* tete, int v) {
         nouv->pre = dernier;
     }
 }
- 
+//FONCTION pour insérer un élément dans la liste
+ void insertElementAtPosition(ListP* tete, int v, int position) {
+    ListP nouv = (ListP)malloc(sizeof(Liste));
+    nouv->info = v;
+    nouv->pre = NULL;
+    nouv->suiv = NULL;
+
+    if (position <= 1) {
+        // Inserer a la tete
+        nouv->suiv = *tete;
+        if (*tete != NULL) {
+            (*tete)->pre= nouv;
+        }
+        *tete = nouv;
+    } else {
+        // trouver la position
+        ListP cur = *tete;
+        int count = 1;
+
+        while (cur != NULL && count < position) {
+            cur = cur->suiv;
+            count++;
+        }
+
+        // Inserer apres le noeud trouvee (ou a la fin si la position n est pas donnee)
+        if (cur != NULL) {
+            nouv->pre = cur->pre;
+            nouv->suiv = cur;
+            if (cur->pre != NULL) {
+                cur->pre->suiv = nouv;
+            } else {
+                // si la position est a la tete mise a jour tete
+                *tete = nouv;
+            }
+            cur->pre= nouv;
+        } else {
+            ListP l = *tete;
+            while (l->suiv != NULL) {
+                l = l->suiv;
+            }
+            l->suiv = nouv;
+            nouv->pre = l;
+        }
+    }
+}
  
  int main(){
 
